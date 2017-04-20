468ccf2218d596366c508f3b7306d09fa327b9cd
shuolin.peng <shuolin.peng@pjbest.com>
2017-04-20 19:48:30 +0800

    modify softmax

--- autodiff/node/op/softmax.py
+++ autodiff/node/op/softmax.py
@@ -1,46 +1,110 @@
 
+from autodiff.node.node import Node
 from autodiff.node.binary import Binary
+from autodiff.node.unitary import Unitary
 
 import numpy as np
 
 
-class Softmax(Binary):
+class Softmax(Unitary):
 
-    def __init__(self, a, b):
-        assert len(a.shape) == 1 and len(b.shape) <= 1
-        super().__init__(a, b, code='softmax', prior=0)
+    def __init__(self, log_prob):
+        assert len(log_prob.shape) == 1
+        super().__init__(log_prob, 'softmax')
+        # log_prob should be 1-D array
 
-        self._correct_prob_is_idx = not len(b.shape)
-        if not self._correct_prob_is_idx:
-            assert np.size(a) == np.size(b)
+    def __getitem__(self, correct):
+        assert isinstance(correct, Node)
+        return SoftmaxLoss(self, correct)
 
-    def eval_op(self, log_prob, correct_prob):
-        # correct_prob is not invoked in forward calculation
+    def eval_op(self, log_prob):
         log_prob -= np.max(log_prob)
         norm_prob = np.exp(log_prob)
-        return -np.log(norm_prob / np.sum(norm_prob))
+        return norm_prob / np.sum(norm_prob)
 
-    def eval_grad(self, log_prob, correct_prob):
-        if self._correct_prob_is_idx:
-            # correct_prob should be a index of vector
+    def eval_grad(self, log_prob):
+        log_prob -= np.max(log_prob)
+        norm_prob = np.exp(log_prob)
+        return norm_prob / np.sum(norm_prob)
+
+    def backward(self, grad, from_softmax_loss=False):
+
+        if not self._prepare_backward(grad):
+            return
+
+        if from_softmax_loss:
+            pass
+
+
+class SoftmaxLoss(Binary):
+
+    def __init__(self, prob, correct):
+        # correct should be int or 1-D array
+        assert isinstance(prob, Softmax)
+        assert len(prob.shape) == 1 and len(correct.shape) <= 1
+        super().__init__(prob, correct, code='softmax-loss', prior=0)
+
+        self._shape = ()  # loss result is a prob value
+        self._correct_is_idx = not len(correct.shape)
+        if self._correct_is_idx:
+            assert np.size(prob) == np.size(correct)
+
+    def eval_op(self, prob, correct):
+
+        # actually, correct is not invoked in forward calculation
+        if self._correct_is_idx:
+            # correct should be a index of vector
             # which indicate the expected correct class
-            assert isinstance(correct_prob, int)
-            assert 0 < correct_prob < np.size(log_prob)
+            assert isinstance(correct, int)
+            assert 0 <= correct < np.size(prob)
+            return -np.log(prob[correct])
         else:
-            # correct_prob should be binary(0/1) prob vector
+            # correct should be binary(0/1) prob vector
             # which index of 1 indicated the expected correct class
-            assert np.min(correct_prob), np.max(correct_prob) == (0, 1)
-            assert np.sum(correct_prob) == 1
+            assert np.min(correct), np.max(correct) == (0, 1)
+            assert np.sum(correct) == 1
+            return -np.log(prob[np.argmax(correct)])
 
-        log_prob -= np.max(log_prob)
-        norm_prob = np.exp(log_prob)
-        norm_prob /= np.sum(norm_prob)
+    def eval_grad(self, prob, correct):
+        if self._correct_is_idx:
+            # correct is a index of vector
+            # which indicate the expected correct class
+            assert isinstance(correct, int)
+            assert 0 <= correct < np.size(prob)
 
-        if self._correct_prob_is_idx:
-            correct_idx = correct_prob
-            correct_prob = np.zeros(norm_prob.shape)
-            correct_prob[correct_idx] = 1
+            # equals to:
+            # prob -= [..., 1, ...]
+            prob[correct] -= 1.
 
-        # the error between the norm_prob and correct_prob is the gradient of softmax
-        return norm_prob - correct_prob, 0.  # correct_prob is ignored in backward propagation
+        else:
+            # correct is binary(0/1) prob vector like [..., 1, ...]
+            # which index of 1 indicated the expected correct class
+            assert np.min(correct), np.max(correct) == (0, 1)
+            assert np.sum(correct) == 1
+
+            prob -= correct
+
+        # the error of the prob is the gradient of softmax
+        return prob, 0.0  # gradient of correct is ignore
+        # because it is not invoked in forward
+
+    def backward(self, grad):
+
+        if not self._prepare_backward(grad):
+            return
+
+        assert isinstance(grad, (int, float))
+
+        # keep _left_grad and _right_grad be zeros
+
+        l_result, r_result = self._left.result, self._right.result
+        l_grad, _ = self.eval_grad(np.copy(l_result), r_result)
+
+        self._left.backward(l_grad, True)
+
+
+
+
+
+
 
